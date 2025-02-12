57193a945764a03a5ba60a320641d4ef8b490737
Sharpsharky <maciej.jasielewicz@gmail.com>
2025-02-13 00:56:33 +0100

    Small layout fixes

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -41,25 +41,25 @@
             this.label4 = new System.Windows.Forms.Label();
             this.groupBoxBottom = new System.Windows.Forms.GroupBox();
             this.btnSort = new System.Windows.Forms.Button();
-            this.table = new System.Windows.Forms.TableLayoutPanel();
             this.groupBox1.SuspendLayout();
             this.groupBoxBottom.SuspendLayout();
-            this.table.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtTask
             // 
             this.txtTask.AccessibleName = "txtTask";
-            this.txtTask.Location = new System.Drawing.Point(41, 24);
-            this.txtTask.MaxLength = 30;
+            this.txtTask.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTask.Location = new System.Drawing.Point(48, 20);
+            this.txtTask.MaxLength = 120;
             this.txtTask.Name = "txtTask";
-            this.txtTask.Size = new System.Drawing.Size(221, 20);
+            this.txtTask.Size = new System.Drawing.Size(214, 20);
             this.txtTask.TabIndex = 0;
             // 
             // btnAddTask
             // 
             this.btnAddTask.AccessibleName = "btnAddTask";
-            this.btnAddTask.Location = new System.Drawing.Point(193, 52);
+            this.btnAddTask.Location = new System.Drawing.Point(193, 48);
             this.btnAddTask.Name = "btnAddTask";
             this.btnAddTask.Size = new System.Drawing.Size(69, 21);
             this.btnAddTask.TabIndex = 1;
@@ -70,7 +70,9 @@
             // 
             this.lstTasks.AccessibleName = "lstTasks";
             this.lstTasks.AllowDrop = true;
-            this.lstTasks.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lstTasks.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lstTasks.BackColor = System.Drawing.SystemColors.Window;
             this.lstTasks.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
             this.lstTasks.FormattingEnabled = true;
@@ -89,6 +91,7 @@
             // btnRemoveTask
             // 
             this.btnRemoveTask.AccessibleName = "btnRemoveTask";
+            this.btnRemoveTask.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnRemoveTask.Location = new System.Drawing.Point(166, 230);
             this.btnRemoveTask.Name = "btnRemoveTask";
             this.btnRemoveTask.Size = new System.Drawing.Size(93, 23);
@@ -101,7 +104,7 @@
             this.cmbStatus.AccessibleName = "cmbStatus";
             this.cmbStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbStatus.FormattingEnabled = true;
-            this.cmbStatus.Location = new System.Drawing.Point(9, 52);
+            this.cmbStatus.Location = new System.Drawing.Point(9, 48);
             this.cmbStatus.Name = "cmbStatus";
             this.cmbStatus.Size = new System.Drawing.Size(179, 21);
             this.cmbStatus.TabIndex = 4;
@@ -109,6 +112,7 @@
             // btnUpdateStatus
             // 
             this.btnUpdateStatus.AccessibleName = "btnUpdateStatus";
+            this.btnUpdateStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.btnUpdateStatus.Location = new System.Drawing.Point(9, 230);
             this.btnUpdateStatus.Name = "btnUpdateStatus";
             this.btnUpdateStatus.Size = new System.Drawing.Size(93, 23);
@@ -119,43 +123,45 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.Size = new System.Drawing.Size(39, 15);
             this.label1.TabIndex = 6;
             this.label1.Text = "Label:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Calibri", 11F);
             this.label2.Location = new System.Drawing.Point(6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.Size = new System.Drawing.Size(40, 18);
             this.label2.TabIndex = 7;
             this.label2.Text = "Tasks";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 10);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(501, 32);
+            this.label3.Size = new System.Drawing.Size(268, 20);
             this.label3.TabIndex = 8;
             this.label3.Text = "To-Do List";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // groupBox1
             // 
-            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.txtTask);
-            this.groupBox1.Controls.Add(this.btnAddTask);
-            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.cmbStatus);
-            this.groupBox1.Location = new System.Drawing.Point(119, 40);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnAddTask);
+            this.groupBox1.Location = new System.Drawing.Point(8, 49);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(268, 84);
             this.groupBox1.TabIndex = 9;
@@ -163,23 +169,26 @@
             // 
             // label4
             // 
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Calibri", 11F);
             this.label4.Location = new System.Drawing.Point(6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(72, 17);
+            this.label4.Size = new System.Drawing.Size(67, 17);
             this.label4.TabIndex = 11;
             this.label4.Text = "New Task";
             this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // groupBoxBottom
             // 
-            this.groupBoxBottom.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.groupBoxBottom.Controls.Add(this.btnSort);
+            this.groupBoxBottom.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxBottom.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.groupBoxBottom.Controls.Add(this.label2);
-            this.groupBoxBottom.Controls.Add(this.lstTasks);
             this.groupBoxBottom.Controls.Add(this.btnRemoveTask);
             this.groupBoxBottom.Controls.Add(this.btnUpdateStatus);
-            this.groupBoxBottom.Location = new System.Drawing.Point(119, 140);
+            this.groupBoxBottom.Controls.Add(this.btnSort);
+            this.groupBoxBottom.Controls.Add(this.lstTasks);
+            this.groupBoxBottom.Location = new System.Drawing.Point(8, 139);
             this.groupBoxBottom.Name = "groupBoxBottom";
             this.groupBoxBottom.Size = new System.Drawing.Size(268, 263);
             this.groupBoxBottom.TabIndex = 10;
@@ -188,37 +197,27 @@
             // btnSort
             // 
             this.btnSort.AccessibleName = "btnUpdateStatus";
+            this.btnSort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSort.AutoSize = true;
+            this.btnSort.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnSort.Location = new System.Drawing.Point(190, 0);
+            this.btnSort.MaximumSize = new System.Drawing.Size(120, 23);
+            this.btnSort.MinimumSize = new System.Drawing.Size(69, 23);
             this.btnSort.Name = "btnSort";
             this.btnSort.Size = new System.Drawing.Size(69, 23);
             this.btnSort.TabIndex = 9;
             this.btnSort.Text = "Sort";
             this.btnSort.UseVisualStyleBackColor = true;
             // 
-            // table
-            // 
-            this.table.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.table.ColumnCount = 1;
-            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.table.Controls.Add(this.groupBoxBottom, 0, 2);
-            this.table.Controls.Add(this.groupBox1, 0, 1);
-            this.table.Controls.Add(this.label3, 0, 0);
-            this.table.Location = new System.Drawing.Point(-109, 0);
-            this.table.Name = "table";
-            this.table.RowCount = 3;
-            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.06417F));
-            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75.93583F));
-            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 276F));
-            this.table.Size = new System.Drawing.Size(507, 410);
-            this.table.TabIndex = 11;
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(289, 410);
-            this.Controls.Add(this.table);
+            this.ClientSize = new System.Drawing.Size(284, 411);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBoxBottom);
+            this.MinimumSize = new System.Drawing.Size(300, 450);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainForm";
@@ -227,8 +226,6 @@
             this.groupBox1.PerformLayout();
             this.groupBoxBottom.ResumeLayout(false);
             this.groupBoxBottom.PerformLayout();
-            this.table.ResumeLayout(false);
-            this.table.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -247,7 +244,6 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.GroupBox groupBoxBottom;
-        private System.Windows.Forms.TableLayoutPanel table;
         private System.Windows.Forms.Button btnSort;
     }
 }
